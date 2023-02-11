96bd4dd987990b363933c53b45b729ac59f38001
Ellahhh <oghenekaro57@gmail.com>
2023-02-11 18:07:45 +0100

    created the about us page, added the images needed and grouped it

--- Ben_Frontend/src/pages/About.jsx
+++ Ben_Frontend/src/pages/About.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import Header from './About_Section/Hero'
+import Values from './About_Section/Values'
+import Quotes from './About_Section/Quote'
+import Believe from './About_Section/Believe'
 
 const About = () => {
   return (
-    <div className='bg-red-700 text-white text-xl'>About</div>
+    <div className="App flex flex-col gap-20">
+      <Header />
+      <Values />
+      <Quotes />
+      <Believe />
+    </div>
   )
 }
 
