d0f7ae624c36051f5d48a5983460ae568eb5d65d
oghene-ella <oghenekaro57@gmail.com>
2023-12-24 09:28:14 -0500

    updated the map, about us page, visitation page and the home page

--- Ben_Frontend/src/pages/Map_Section/Map1.jsx
+++ Ben_Frontend/src/pages/Map_Section/Map1.jsx
@@ -1,8 +1,7 @@
 function App() {
   return (
     <div className="App">
-      <iframe src="https://www.google.com/maps/embed?pb=!1m26!1m12!1m3!1d2027407.8133999326!2d3.9739069067529376!3d7.0350199577558605!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m11!3e0!4m3!3m2!1d7.345981399999999!2d3.8553517!4m5!1s0x1046c55555555555%3A0x3b367147c4f6d91!2sEdo%20University%2C%20Iyamho%2C%20689M%2BPP5%2C%20312107%2C%20Okpella%2C%20Edo!3m2!1d7.219255599999999!2d6.3343506!5e0!3m2!1sen!2sng!4v1677744704766!5m2!1sen!2sng" loading="lazy" className="w-full m-auto h-[50rem] ">
-      </iframe>
+      <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3958.189085232828!2d6.331775675887084!3d7.21926091468546!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x1046c55555555555%3A0x3b367147c4f6d91!2sEdo%20University%2C%20Iyamho!5e0!3m2!1sen!2sus!4v1703425051662!5m2!1sen!2sus" className="w-full m-auto h-[50rem] " allowFullScreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
     </div>
   );
 }
